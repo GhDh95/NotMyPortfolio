77cddc4de520245c4a5e044ea6af37b21731b979
Ghassen Dhaoui <GhassenDhaoui@outlook.com>
2023-05-01 00:41:37 +0200

    final

---
+++ components/experience.php
@@ -0,0 +1,56 @@
+<div id="experience" class="w-full bg-gray-50 mt-20 rounded-lg p-1">
+<p class="text-gray-500">Experience</p>
+    <ol
+        class="border-l border-neutral-300 dark:border-neutral-500 md:flex md:justify-center md:gap-6 md:border-l-0 md:border-t">
+        <!--First item-->
+        <li>
+            <div class="flex-start flex items-center pt-2 md:block md:pt-0">
+                <div
+                    class="-ml-[5px] mr-3 h-[9px] w-[9px] rounded-full bg-neutral-300 dark:bg-neutral-500 md:-mt-[5px] md:ml-0 md:mr-0"></div>
+                <p class="mt-2 text-sm text-neutral-500 dark:text-neutral-300">
+                    Oct 2019 - Dec 2022
+                </p>
+            </div>
+            <div class="ml-4 mt-2 pb-5 md:ml-0">
+                <h4 class="mb-1.5 text-xl font-semibold">Rotaract Club Tunis</h4>
+                <p class="mb-3 text-neutral-500 dark:text-neutral-300">
+                    Membre
+                </p>
+            </div>
+        </li>
+
+        <li>
+            <div class="flex-start flex items-center pt-2 md:block md:pt-0">
+                <div
+                    class="-ml-[5px] mr-3 h-[9px] w-[9px] rounded-full bg-neutral-300 dark:bg-neutral-500 md:-mt-[5px] md:ml-0 md:mr-0"></div>
+                <p class="mt-2 text-sm text-neutral-500 dark:text-neutral-300">
+                    Juin 2020 - Jul 2021
+                </p>
+            </div>
+            <div class="ml-4 mt-2 pb-5 md:ml-0">
+                <h4 class="mb-1.5 text-xl font-semibold">Rotaract Club Tunis</h4>
+                <p class="mb-3 text-neutral-500 dark:text-neutral-300">
+                    Cheffe de la Commission action interieure.
+                </p>
+            </div>
+        </li>
+
+        <li>
+            <div class="flex-start flex items-center pt-2 md:block md:pt-0">
+                <div
+                    class="-ml-[5px] mr-3 h-[9px] w-[9px] rounded-full bg-neutral-300 dark:bg-neutral-500 md:-mt-[5px] md:ml-0 md:mr-0"></div>
+                <p class="mt-2 text-sm text-neutral-500 dark:text-neutral-300">
+                    Fev 2022 - Juin 2022
+                </p>
+            </div>
+            <div class="ml-4 mt-2 pb-5 md:ml-0">
+                <h4 class="mb-1.5 text-xl font-semibold">Lions Club Tunis</h4>
+                <p class="mb-3 text-neutral-500 dark:text-neutral-300">
+                    Membre fondatrice.
+                </p>
+            </div>
+        </li>
+
+    </ol>
+
+</div>
---
+++ components/timeline.php
@@ -0,0 +1,25 @@
+<div id="education" class="w-full mt-20 ">
+
+   <div class="p-1 border border-black">
+       <div class="p-2 bg-gradient-to-r from-fourth to-primary ">
+           <ol class="relative border-l border-gray-200 bg-gray-50">
+               <li class="mb-10 ml-4">
+                   <div class="absolute w-3 h-3 bg-gray-50 rounded-full mt-1.5 -left-1.5 border border-primary dark:border-gray-900 dark:bg-gray-700"></div>
+                   <time class="border-b border-gray-300 mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">2021 - Present</time>
+                   <h3 class="text-lg font-semibold text-gray-900 dark:text-white">Esprit School of Business (ESB)</h3>
+                   <p class="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">Licence en Business Computing.</p>
+               </li>
+               <li class=" ml-4">
+                   <div class="absolute w-3 h-3 bg-gray-50 rounded-full mt-1.5 -left-1.5 border border-primary dark:border-gray-900 dark:bg-gray-700"></div>
+                   <time class="border-b border-gray-300 mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">2019</time>
+                   <h3 class="text-lg font-semibold text-gray-900 dark:text-white">Lycee El Wafa</h3>
+                   <p class="text-base font-normal text-gray-500 dark:text-gray-400">Baccalaureat Scientifique.</p>
+               </li>
+           </ol>
+       </div>
+   </div>
+
+
+
+
+</div>
