be9f4b464bc44e4dda63c47e366640e1f455b625
Ghassen Dhaoui <GhassenDhaoui@outlook.com>
2023-05-01 00:29:44 +0200

    final

--- components/main-section.php
+++ components/main-section.php
@@ -1,9 +1,9 @@
 <div id="main-section" class="w-full bg-sky-950 p-2 flex justify-center  mt-10">
-    <div class=" flex flex-row p-2 space-x-2 hover:scale-105 transition ease-in duration-300">
+    <div class=" flex flex-col md:flex md:flex-row p-2 space-x-2 hover:scale-105 transition ease-in duration-300">
         <div>
-            <img class="w-72 h-72 rounded-full" src="public/ghada.jpg" alt="photo de profil">
+            <img class="w-72 h-72 md:rounded-full" src="public/ghada.jpg" alt="photo de profil">
         </div>
-        <div class="rounded-lg w-72 flex flex-col items-center py-2 space-y-2 text-sixth">
+        <div class="rounded-lg md:w-72 flex flex-col items-center py-2 space-y-2 text-sixth">
             <p class="text-2xl font-semibold">Hi!</p>
             <p>Je suis
                 <span class="text-fourth font-semibold underline">
